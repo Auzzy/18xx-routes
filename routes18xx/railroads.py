bd35013b145e7a5645aaf272e28f0d0388df23d8
Austin <mathfreak65@gmail.com>
2020-06-17 23:28:47 +0000

    Load railroad and train info from JSON.
    To allow for different definitions between games. It also allows for
    expressive capturing of properties of each, should they be needed. For
    example, in cluded in this commit is the ability to name trains, and
    defining nicknames for each railroad.

--- routes18xx/railroads.py
+++ routes18xx/railroads.py
@@ -1,80 +1,23 @@
 import csv
 import itertools
+import json
 
+from routes18xx import get_data_file, trains
 from routes18xx.tokens import Station
 from routes18xx.cell import CHICAGO_CELL, Cell
 
+_RAILROADS_FILENAME = "railroads.json"
+
 FIELDNAMES = ("name", "trains", "stations", "chicago_station_exit_coord")
-
-TRAIN_TO_PHASE = {
-    (2, 2): 1,
-    (3, 5): 2,
-    (4, 4): 2,
-    (4, 6): 3,
-    (5, 5): 3,
-    (6, 6): 4,
-    (7, 8): 4
-}
-
-RAILROAD_HOME_CITIES = {
-    "Baltimore & Ohio": "G19",
-    "Illinois Central": "K3",
-    "New York Central": "D20",
-    "Chesapeake & Ohio": "I15",
-    "Erie": "E21",
-    "Grand Trunk": "B16",
-    "Pennsylvania": "F20"
-}
-
-REMOVABLE_RAILROADS = {
-    "Chesapeake & Ohio",
-    "Erie",
-    "Pennsylvania"
-}
-
-class Train(object):
-    @staticmethod
-    def create(train_str):
-        parts = train_str.split("/")
-        collect = int(parts[0].strip())
-        visit = int((parts[0] if len(parts) == 1 else parts[1]).strip())
-
-        if (collect, visit) not in TRAIN_TO_PHASE:
-            train_str = ", ".join(sorted(TRAIN_TO_PHASE.keys()))
-            raise ValueError("Invalid train string found. Got ({}, {}), but expected one of {}".format(collect, visit, train_str))
-        
-        return Train(collect, visit, TRAIN_TO_PHASE[(collect, visit)])
-
-    def __init__(self, collect, visit, phase):
-        self.collect = collect
-        self.visit = visit
-        self.phase = phase
-
-    def __str__(self):
-        if self.collect == self.visit:
-            return str(self.collect)
-        else:
-            return "{} / {}".format(self.collect, self.visit)
-
-    def __hash__(self):
-        return hash((self.phase, self.collect, self.visit))
-
-    def __eq__(self, other):
-        return isinstance(other, Train) and \
-                self.phase == other.phase and \
-                self.collect == other.collect and \
-                self.visit == other.visit
 
 class Railroad(object):
     @staticmethod
-    def create(name, trains_str):
-        trains = [Train.create(train_str) for train_str in trains_str.split(",") if train_str] if trains_str else []
+    def create(name, railroad_trains):
+        return Railroad(name, railroad_trains)
 
-        return Railroad(name, trains)
-
-    def __init__(self, name, trains):
+    def __init__(self, name, railroad_trains):
         self.name = name
-        self.trains = trains
+        self.trains = railroad_trains
         self.has_mail_contract = False
 
     def assign_mail_contract(self):
@@ -101,40 +44,49 @@ class RemovedRailroad(Railroad):
     def is_removed(self):
         return True
 
+def _load_railroad_info(game):
+    with open(get_data_file(game, _RAILROADS_FILENAME)) as railroads_file:
+        return json.load(railroads_file)
 
 def load_from_csv(game, board, railroads_filepath):
     with open(railroads_filepath, newline='') as railroads_file:
         return load(game, board, csv.DictReader(railroads_file, fieldnames=FIELDNAMES, delimiter=';', skipinitialspace=True))
 
 def load(game, board, railroads_rows):
+    railroad_info = _load_railroad_info(game)
+    train_info = trains.load_train_info(game)
+
     railroads = {}
     for railroad_args in railroads_rows:
+        name = railroad_args["name"]
+        info = railroad_info.get(name, {})
+        if not info:
+            raise ValueError("Unrecognized railroad name: {}".format(name))
+
         trains_str = railroad_args.get("trains")
         if trains_str and trains_str.lower() == "removed":
             name = railroad_args["name"]
-            if name not in REMOVABLE_RAILROADS:
+            if info.get("is_removable"):
                 raise ValueError("Attempted to remove a non-removable railroad.")
 
             railroad = RemovedRailroad.create(name)
         else:
-            railroad = Railroad.create(railroad_args["name"], trains_str)
+            railroad_trains = trains.convert(train_info, trains_str)
+            railroad = Railroad.create(railroad_args["name"], railroad_trains)
 
         if railroad.name in railroads:
             raise ValueError(f"Found multiple {railroad.name} definitions.")
 
         railroads[railroad.name] = railroad
 
-        if railroad.name not in RAILROAD_HOME_CITIES:
-            raise ValueError("Unrecognized railroad name: {}".format(railroad.name))
-
         # Place the home station
-        board.place_station(RAILROAD_HOME_CITIES[railroad.name], railroad)
+        board.place_station(info["home"], railroad)
 
         station_coords_str = railroad_args.get("stations")
         if station_coords_str:
             station_coords = [coord.strip() for coord in station_coords_str.split(",")]
             for coord in station_coords:
-                if coord and coord != RAILROAD_HOME_CITIES[railroad.name] and Cell.from_coord(coord) != CHICAGO_CELL:
+                if coord and coord != info["home"] and Cell.from_coord(coord) != CHICAGO_CELL:
                     board.place_station(coord, railroad)
 
             if str(CHICAGO_CELL) in station_coords:
@@ -143,5 +95,10 @@ def load(game, board, railroads_rows):
                     raise ValueError("Chicago is listed as a station for {}, but not exit side was specified.".format(railroad.name))
 
                 board.place_chicago_station(railroad, int(chicago_station_exit_coord))
+
+    for name, info in railroad_info.items():
+        if name in railroads:
+            for nickname in info.get("nicknames", []):
+                railroads[nickname] = railroads[name]
 
     return railroads
