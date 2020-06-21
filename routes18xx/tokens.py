2ea34d03b4400cbafb2812f087a42885a3853831
Austin <mathfreak65@gmail.com>
2020-06-21 13:06:23 +0000

    Route phase management through the game object.
    Since it's already present in many places, and will be needed in many
    places, it's a natural place to store this info. Also a natural place to
    load it, and provide other game specific information. And provides an
    easy way to handle each games' different phase progression, as well as
    comparing non-integer phases.

--- routes18xx/tokens.py
+++ routes18xx/tokens.py
@@ -8,16 +8,17 @@ class Station(Token):
 
 class PrivateCompanyToken(Token):
     @staticmethod
-    def place(cell, railroad, properties):
+    def place(name, cell, railroad, properties):
         if railroad.is_removed:
             raise ValueError("A removed railroad cannot place a private company's token: {}".format(railroad.name))
 
         return PrivateCompanyToken(cell, railroad)
 
-    def __init__(self, cell, railroad, bonus=0):
+    def __init__(self, name, cell, railroad, bonus=0):
         super().__init__(cell, railroad)
 
+        self.name = name
         self.bonus = bonus
 
-    def value(self, railroad, phase):
-        return self.bonus if phase != 4 and self.railroad == railroad else 0
+    def value(self, game, railroad):
+        return self.bonus if not game.private_is_closed(self.name) and self.railroad == railroad else 0
