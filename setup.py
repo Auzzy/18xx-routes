28399e524063d2efa801aab8e95f29886667274e
Austin <mathfreak65@gmail.com>
2020-06-27 12:57:33 +0000

    Update setup.py for release.
    This includes switching to use the entry point model of script
    distribution. The contents of bin/calc-route.py are moved into
    find_best_routes.py as the main() function.

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 with open("README.md", "r") as readme_file:
     long_description = readme_file.read()
@@ -7,17 +7,23 @@ setup(
     name='routes-18xx',
     version='0.1',
     author="Austin Noto-Moniz",
-    author_email="metalnut4@netscape.net",
+    author_email="mathfreak65@gmail.com",
     description="Library for caluclating routes in 18XX train games.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Auzzy/18xx-routes",
-    packages=['routes18xx'],
-    package_data={"routes18xx": ["data/*"]},
+    packages=find_packages(),
+    package_data={"routes18xx": ["data/1846/*", "data/1889/*"]},
     classifiers=[
         "Development Status :: 4 - Beta",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License"
-    ]
+    ],
+    python_requires='>=3.6',
+    entry_points={
+        "console_scripts": [
+            "calc-routes = routes18xx.find_best_routes:main"
+        ]
+    }
 )
