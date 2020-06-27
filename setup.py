697de444b4ebd8031cb4c92171ec0fdb6879d16f
Austin <mathfreak65@gmail.com>
2020-06-27 15:02:53 +0000

    Add a README.
    For now at least, the README serves as its canonical documentation. Maybe
    I'll set up an external site at some point (e.g. readthedocs), but this
    is good enough for now.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-with open("README.md", "r") as readme_file:
+with open("README.rst", "r") as readme_file:
     long_description = readme_file.read()
 
 setup(
@@ -8,9 +8,9 @@ setup(
     version='0.1',
     author="Austin Noto-Moniz",
     author_email="mathfreak65@gmail.com",
-    description="Library for caluclating routes in 18XX train games.",
+    description="Library for caluclating routes in 18xx train games.",
     long_description=long_description,
-    long_description_content_type="text/markdown",
+    long_description_content_type="text/x-rst",
     url="https://github.com/Auzzy/18xx-routes",
     packages=find_packages(),
     package_data={"routes18xx": ["data/1846/*", "data/1889/*"]},
