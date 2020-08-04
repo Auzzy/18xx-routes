1c9cf690195a838710f29de509aea46264331ec1
Austin <mathfreak65@gmail.com>
2020-08-04 23:46:58 +0000

    Allow closing railroads.
    If the game supports railroads closing, then allow them to be indicated
    closed. Their reserved station slots will be forfeited.
    
    As of the current supported games, a closed railroad is the same as a
    removed railroad.

--- routes18xx/rules.py
+++ routes18xx/rules.py
@@ -4,12 +4,15 @@ class Rules:
         rules_json = game_json.get("rules", {})
         return Rules(
             rules_json.get("towns", {}),
+            rules_json.get("railroads", {}),
             rules_json.get("stations", {}),
             rules_json.get("privates", {}))
 
-    def __init__(self, town_rules, station_rules, privates_rules):
+    def __init__(self, town_rules, railroad_rules, station_rules, privates_rules):
         self.towns_omit_from_limit = town_rules.get("omit_from_limit", False)
+
+        self.railroads_can_close = railroad_rules.get("can_close", True)
 
         self.stations_reserved_until = station_rules.get("reserved_until")
 
-        self.privates_close = privates_rules.get("close")
+        self.privates_close = privates_rules.get("close", {})
